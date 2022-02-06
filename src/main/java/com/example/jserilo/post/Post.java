bdf4ab597694725ade574de5ba08026f78314252
Kabilaw <kabilaw.101@gmail.com>
2022-02-07 00:41:50 +0800

    Configure Post Entity

--- src/main/java/com/example/jserilo/post/Post.java
+++ src/main/java/com/example/jserilo/post/Post.java
@@ -1,6 +1,33 @@
 package com.example.jserilo.post;
 
-public record Post(Integer id, String title) {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Post {
     
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String title;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
 }
